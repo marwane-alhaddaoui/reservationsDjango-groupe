09af1ff6401035a8c05103a584d6546d8c42f392
Sérgio Borges <sergio_borges82@hotmail.com>
2025-01-13 22:24:42 +0100

    Register other models to django admin panel

--- reservations/catalogue/admin.py
+++ reservations/catalogue/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Artist
+from .models import Artist, Locality, Location, Price, Reservation, Show, Type
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 
@@ -7,6 +7,12 @@ from catalogue.models import UserMeta
 
 # Register your models here.
 admin.site.register(Artist)
+admin.site.register(Locality)
+admin.site.register(Location)
+admin.site.register(Price)
+admin.site.register(Reservation)
+admin.site.register(Show)
+admin.site.register(Type)
 
 # Define an inline admin descriptor for Employee model
 # which acts a bit like a singleton
