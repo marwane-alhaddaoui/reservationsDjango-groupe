4e937a267c753010bd38076798bb54d417179152
Bounou Khalid (ZPZ PolBru) <Khalidbounou@gmail.com>
2025-05-12 23:36:02 +0200

    Correction of Autorisation in backend

--- reservations/accounts/views.py
+++ reservations/accounts/views.py
@@ -411,7 +411,6 @@ def check_auth(request):
 
 class UserMetaDetailView(APIView):
     authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
 
     def get(self, request, user_id):
         if request.user.id != user_id:
