78c5564fa16f826a9e9aad8dc7d147f682343356
Bounou Khalid (ZPZ PolBru) <Khalidbounou@gmail.com>
2025-05-13 19:49:26 +0200

    Panel Admin - Update

--- reservations/accounts/admin.py
+++ reservations/accounts/admin.py
@@ -1,3 +1,59 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
+from catalogue.models.cart import Cart, CartItem
+from catalogue.models.price import Price
+from catalogue.models.representation import Representation
+from catalogue.models.reservation import Reservation
+from catalogue.models import ArtistType, Show, Review
 
-# Register your models here.
+# Unregister the default User model
+admin.site.unregister(User)
+
+# Register the User model with your custom UserAdmin
+@admin.register(User)
+class UserAdmin(DefaultUserAdmin):
+    list_display = ('username', 'email', 'is_staff', 'is_active')
+    search_fields = ('username', 'email')
+    list_filter = ('is_staff', 'is_active')
+
+@admin.register(ArtistType)
+class ArtistTypeAdmin(admin.ModelAdmin):
+    list_display = ('artist', 'type')
+    list_filter = ('type',)
+
+@admin.register(Show)
+class ShowAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at')
+    search_fields = ('title',)
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('user', 'show', 'stars', 'review')
+    list_filter = ('stars',)
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('user', 'created_at')
+    search_fields = ('user__username',)
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ('cart', 'representation', 'price', 'quantity')
+    list_filter = ('price__type',)
+
+@admin.register(Price)
+class PriceAdmin(admin.ModelAdmin):
+    list_display = ('type', 'price')
+    search_fields = ('type',)
+
+@admin.register(Representation)
+class RepresentationAdmin(admin.ModelAdmin):
+    list_display = ('show', 'schedule', 'location')
+    list_filter = ('location',)
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ('user', 'booking_date', 'status')
+    list_filter = ('status',)
+    search_fields = ('user__username',)
