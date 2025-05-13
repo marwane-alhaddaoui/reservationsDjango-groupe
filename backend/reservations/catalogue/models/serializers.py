ac06af3c56f96f3b4e82708410be005b2473d0df
Marwane Al haddaoui <m.alhaddaoui.000991@stu.brucity.education>
2025-05-14 01:40:33 +0200

    Switch ArtistSerializer to ModelSerializer to fix get_links error

--- backend/reservations/catalogue/models/serializers.py
+++ backend/reservations/catalogue/models/serializers.py
@@ -1,16 +1,7 @@
 from rest_framework import serializers
 from .artist import Artist
-from rest_framework.reverse import reverse
 
-class ArtistSerializer(serializers.HyperlinkedModelSerializer):
-    links = serializers.SerializerMethodField()
-
+class ArtistSerializer(serializers.ModelSerializer):
     class Meta:
         model = Artist
-        fields = ['id', 'firstname', 'lastname', 'links']
-def get_links(self, obj):
-        request = self.context.get('request')
-        return {
-            'self': reverse('catalogue:artist-detail', kwargs={'pk': obj.id}, request=request),
-            'all_artists': reverse('catalogue:artist-list', request=request),
-        }
+        fields = ['id', 'firstname', 'lastname']
