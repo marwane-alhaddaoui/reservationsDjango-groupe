7218b5fd64200e6e664f7b081a63813615e1fa26
Marwane Al haddaoui <m.alhaddaoui.000991@stu.brucity.education>
2025-05-14 01:01:11 +0200

    Use absolute import in catalogue/api_urls; add __init__.py

--- backend/reservations/catalogue/api_urls.py
+++ backend/reservations/catalogue/api_urls.py
@@ -1,15 +1,12 @@
 # backend/reservations/catalogue/api_urls.py
-
 from django.urls import path
-# import des classes correctes :
-from ..api.catalogue.views import (
+# import absolu depuis la racine du projet DJango
+from api.catalogue.views import (
     ArtistListCreateView,
     ArtistRetrieveUpdateDestroyView
 )
 
 urlpatterns = [
-    # liste + création
-    path('', ArtistListCreateView.as_view(), name='artist-list'),
-    # détails / maj / suppression
+    path('',     ArtistListCreateView.as_view(),              name='artist-list'),
     path('<int:pk>/', ArtistRetrieveUpdateDestroyView.as_view(), name='artist-detail'),
 ]
